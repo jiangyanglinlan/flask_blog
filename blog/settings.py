bfe1600b9b820a859cc817c2fd40c61496965ce6
jiangyang <1815212328@qq.com>
2018-10-14 22:27:39 +0800

    settings 配置

---
+++ blog/settings.py
@@ -0,0 +1,44 @@
+import os
+
+
+basedir = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
+
+
+class BaseConfig:
+    SECRET_KEY = os.getenv('SECRET_KEY', 'secret string')
+
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    # email 配置
+    MAIL_SERVER = os.getenv('MAIL_SERVER')
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.getenv('MAIL_USERNAME')
+    MAIL_PASSWORD = os.getenv('MAIL_PASSWORD')
+    MAIL_DEFAULT_SENDER = ('上海堡垒', MAIL_USERNAME)
+
+    BLOG_EMAIL = os.getenv('BLOG_EMAIL')
+    BLOG_POST_PER_PAGE = 10
+    BLOG_MANAGE_POST_PER_PAGE = 15
+    BLOG_COMMENT_PER_PAGE = 15
+
+
+class DevelopmentConfig(BaseConfig):
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'data-dev.db')
+
+
+class TestingConfig(BaseConfig):
+    TESTING = True
+    WTF_CSRF_ENABLED = False
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'
+
+
+class ProductionConfig(BaseConfig):
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///' + os.path.join(basedir, 'data.db'))
+
+
+config = {
+    'development': DevelopmentConfig,
+    'testing': TestingConfig,
+    'production': ProductionConfig,
+}
