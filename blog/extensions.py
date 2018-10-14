7d875f8015ba5d05be648f3b794ff0d615917564
jiangyang <1815212328@qq.com>
2018-10-14 22:27:05 +0800

    扩展类实例化

---
+++ blog/extensions.py
@@ -0,0 +1,8 @@
+from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+
+bootstrap = Bootstrap()
+db = SQLAlchemy()
+migrate = Migrate()
