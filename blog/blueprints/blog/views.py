6929e23e19ea70be91a6932858f9092f0d2d6395
jiangyang <1815212328@qq.com>
2018-10-18 21:25:01 +0800

    发表评论以及邮件提醒新评论

--- blog/blueprints/blog/views.py
+++ blog/blueprints/blog/views.py
@@ -1,8 +1,11 @@
 
-from flask import render_template, request, current_app
+from flask import render_template, request, current_app, flash, redirect, url_for
 
 from . import blog_bp
 from ...models import Category, Post, Comment
+from ...forms import CommentForm
+from ...extensions import db
+from ...email import send_new_comment_email
 
 
 @blog_bp.route('/')
@@ -16,11 +19,17 @@ def index():
 
 @blog_bp.route('/about')
 def about():
+    '''
+    博客介绍 
+    '''
     return render_template('blog/about.html')
 
 
 @blog_bp.route('/category/<int:category_id>')
 def show_category(category_id):
+    '''
+    分类页面 
+    '''
     category = Category.query.get_or_404(category_id)
     page = request.args.get('page', 1, type=int)
     per_page = current_app.config['BLOG_POST_PER_PAGE']
@@ -31,10 +40,36 @@ def show_category(category_id):
 
 @blog_bp.route('/post/<int:post_id>', methods=['GET', 'POST'])
 def show_post(post_id):
+    '''
+    文章详情页面 
+    '''
     post = Post.query.get_or_404(post_id)
     page = request.args.get('page', 1, type=int)
     per_page = current_app.config['BLOG_COMMENT_PER_PAGE']
     pagination = Comment.query.with_parent(post).filter_by(reviewed=True).order_by(
         Comment.timestamp.desc()).paginate(page, per_page)
     comments = pagination.items
-    return render_template('blog/post.html', post=post, pagination=pagination, comments=comments)
+
+    form = CommentForm()
+    from_admin = False
+    reviewed = False
+
+    if form.validate_on_submit():
+        author = form.author.data
+        email = form.email.data
+        body = form.body.data
+        comment = Comment(
+            author=author,
+            email=email,
+            body=body,
+            from_admin=from_admin,
+            post=post,
+            reviewed=reviewed,
+        )
+        db.session.add(comment)
+        db.session.commit()
+        flash('您的评论将在审核通过后显示', 'info')
+        print('start send')
+        send_new_comment_email(post) # 发送提醒邮件给博主
+        return redirect(url_for('blog.show_post', post_id=post_id))
+    return render_template('blog/post.html', post=post, pagination=pagination, comments=comments, form=form)
--- blog/email.py
+++ blog/email.py
@@ -15,20 +15,23 @@ def send_mail(subject, to, html):
     app = current_app._get_current_object() # 获取被代理的真实对象
     message = Message(subject, recipients=[to], html=html)
     thr = Thread(target=_send_async_mail, args=[app, message])
+    thr.start()
     return thr
 
 
 def send_new_comment_email(post):
     post_url = url_for('blog.show_post', post_id=post.id, _external=True) + '#comments'
     send_mail(subject='新评论', to=current_app.config['BLOG_EMAIL'],
-              html=f'<p>文章 <i>{post.title}</i> 有一条新评论, 点击下面的链接查看:</p>'
-              '<p><a href={post_url}>{post_url}</a></p>'
-              '<p><small style="color: #868e96">此邮件无需回复。</small></p>')
+              html='<p>文章 <i>%s</i> 有一条新评论, 点击下面的链接查看:</p>'
+              '<p><a href=%s>%s</a></p>'
+              '<p><small style="color: #868e96">此邮件无需回复。</small></p>'
+                % (post.title, post_url, post_url))
 
 
 def send_new_reply_email(comment):
     post_url = url_for('blog.show_post', post_id=comment.post_id, _external=True) + '#comments'
     send_mail(subject='新回复', to=comment.email,
-              html=f'<p>您在文章 <i>{comment.post.title}</i> 下收到了一条新回复, 点击下面的链接查看:</p>'
-              '<p><a hrerf={post_url}>{post_url}</a></p>'
-              '<p><small style="color: #868e96">此邮件无需回复。</small></p>')
+              html=f'<p>您在文章 <i>%s</i> 下收到了一条新回复, 点击下面的链接查看:</p>'
+              '<p><a href=%s>%s</a></p>'
+              '<p><small style="color: #868e96">此邮件无需回复。</small></p>'
+              % (comment.post.title, post_url, post_url))
