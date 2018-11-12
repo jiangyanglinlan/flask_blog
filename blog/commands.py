51ed3d559b76814780f69e512fbb365b119a0212
jiangyang <1815212328@qq.com>
2018-11-12 19:36:00 +0800

    修复创建虚拟数据出错的问题

--- blog/commands.py
+++ blog/commands.py
@@ -6,17 +6,19 @@ from .models import Admin
 
 def register_commands(app):
     @app.cli.command()
+    @click.option('--username', prompt=True, help='管理员用户名')
+    @click.option('--password', prompt=True, hide_input=True, confirmation_prompt=True, help='管理员密码')
     @click.option('--category', default=10, help='生成分类, 默认数量为 10.')
     @click.option('--post', default=50, help='生成文章, 默认数量为 50.')
     @click.option('--comment', default=200, help='生成评论, 默认数量为 200.')
-    def forge(category, post, comment):
+    def forge(username, password, category, post, comment):
         from .fakes import fake_admin, fake_categories, fake_posts, fake_comments
 
         db.drop_all()
         db.create_all()
 
         click.echo('创建管理员...')
-        fake_admin()
+        fake_admin(username, password)
 
         click.echo(f'生成 {category} 条分类...')
         fake_categories(category)
